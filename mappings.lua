ff9044f92b89778e3789ccd5fe960f56cc89f045
ArthurHowardMorris <53531149+ArthurHowardMorris@users.noreply.github.com>
2023-09-07 15:50:23 +0800

    added path yank

--- mappings.lua
+++ mappings.lua
@@ -5,6 +5,7 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<leader>tm"] = { "<cmd> TableModeToggle <CR>", "Toggle Table Mode"},
+    ["<leader>py"] = { "<cmd> !echo %:p <CR> ", "Path Yank, full path to buffer into system clipboard"}
   },
 }
 
